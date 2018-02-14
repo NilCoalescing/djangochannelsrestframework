6348fb08afbb7d4bccad0afc8333e79cb94fb2bf
Matthaus Woolard <matthaus.woolard@gmail.com>
2018-02-14 12:04:43 +0300

    switch to py.test for tests so we can use async test

--- setup.py
+++ setup.py
@@ -10,13 +10,21 @@ setup(
     description="Helps build a RESTful API on top of WebSockets using channels.",
     long_description=open('README.rst').read(),
     license='BSD',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
-        'Django>=1.8',
-        'channels>=0.17',
+        'Django>=1.11',
+        'channels>=2.0.2',
         'djangorestframework>=3.0'
     ],
+    extras_require={
+        'tests': [
+            'pytest~=3.3',
+            "pytest-django~=3.1",
+            "pytest-asyncio~=0.8",
+            'coverage~=4.4',
+        ],
+    },
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+
+def pytest_configure():
+    settings.configure(
+        INSTALLED_APPS=(
+            'django.contrib.auth',
+            'django.contrib.contenttypes',
+            'django.contrib.sessions',
+            'channels',
+            'tests'
+        ),
+        SECRET_KEY='dog',
+
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+
+        CHANNEL_LAYERS = {
+            'default': {
+                'BACKEND': 'asgiref.inmemory.ChannelLayer',
+                'ROUTING': "tests.routing.channel_routing",
+            },
+        },
+
+        MIDDLEWARE_CLASSES = []
+
+        )
