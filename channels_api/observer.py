8aa0a980f72f3836ebceb7b531aba3919fa8560c
Matthaus Woolard <matthaus.woolard@gmail.com>
2018-03-02 10:21:23 +0300

    add model observers

--- channels_api/observer.py
+++ channels_api/observer.py
@@ -1,26 +1,31 @@
+import threading
+from enum import Enum
 from functools import partial
-from typing import Dict, Any
+from typing import Dict, Any, Type, Set
 
 from asgiref.sync import async_to_sync
 from channels.consumer import AsyncConsumer
 from channels.layers import get_channel_layer
+from django.db.models import Model
+from django.db.models.signals import pre_save, post_save, post_delete, \
+    pre_delete
 from django.dispatch import Signal
 
 
-class Observer:
-    def __init__(self, func, signal: Signal=None, kwargs=None):
-        if kwargs is None:
-            kwargs = {}
+class BaseObserver:
+    def __init__(self, func):
         self.func = func
-        self.signal = signal
-        self.signal_kwargs = kwargs
         self._serializer = None
-        self.signal.connect(
-            self.handle, **self.signal_kwargs
-        )
 
     async def __call__(self, *args, **kwargs):
         return await self.func(*args, **kwargs)
+
+    def __get__(self, parent, objtype):
+
+        if parent is None:
+            return self
+
+        return partial(self.__call__, parent)
 
     def serialize(self, signal, *args, **kwargs) -> Dict[str, Any]:
         message = {}
@@ -32,6 +37,22 @@ class Observer:
 
     def serializer(self, func):
         self._serializer = func
+
+    async def subscribe(self, consumer: AsyncConsumer, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class  Observer(BaseObserver):
+    def __init__(self, func, signal: Signal=None, kwargs=None):
+        super().__init__(func)
+        if kwargs is None:
+            kwargs = {}
+        self.signal = signal
+        self.signal_kwargs = kwargs
+        self._serializer = None
+        self.signal.connect(
+            self.handle, **self.signal_kwargs
+        )
 
     def handle(self, signal, *args, **kwargs):
         message = self.serialize(signal, *args, **kwargs)
@@ -48,20 +69,157 @@ class Observer:
             )
         )
 
-    def __get__(self, parent, objtype):
-
-        if parent is None:
-            return self
-
-        return partial(self.__call__, parent)
-
     async def subscribe(self, consumer: AsyncConsumer, *args, **kwargs):
         await consumer.channel_layer.group_add(
             self.channel_name(*args, **kwargs),
             consumer.channel_name
         )
-        print('subscribed', consumer, self.channel_name(*args, **kwargs), consumer.channel_name)
 
 
 def observer(signal, **kwargs):
     return partial(Observer, signal=signal, kwargs=kwargs)
+
+
+class Action(Enum):
+    CREATE = 'create'
+    UPDATE = 'update'
+    DELETE = 'delete'
+
+
+class ModelObserver(BaseObserver):
+
+    def __init__(self, func, model_cls: Type[Model], **kwargs):
+        super().__init__(func)
+        self.model_cls = model_cls  # type: Type[Model]
+        self._connect()
+
+    def _connect(self):
+        pre_save.connect(self.pre_save_receiver, sender=self.model_cls)
+        post_save.connect(self.post_save_receiver, sender=self.model_cls)
+        pre_delete.connect(self.pre_delete_receiver, sender=self.model_cls)
+        post_delete.connect(self.post_delete_receiver, sender=self.model_cls)
+
+    def pre_save_receiver(self, instance: Model, **kwargs):
+        creating = instance._state.adding
+        self.pre_change_receiver(
+            instance,
+            Action.CREATE if creating else Action.UPDATE
+        )
+
+    def post_save_receiver(self, instance: Model, created: bool, **kwargs):
+        self.post_change_receiver(
+            instance,
+            Action.CREATE if created else Action.UPDATE,
+            **kwargs
+        )
+
+    def pre_delete_receiver(self, instance: Model, **kwargs):
+        self.pre_change_receiver(
+            instance,
+            Action.DELETE
+        )
+
+    def post_delete_receiver(self, instance: Model, **kwargs):
+        self.post_change_receiver(instance, Action.DELETE, **kwargs)
+
+    def pre_change_receiver(self, instance: Model, action: Action):
+        """
+        Entry point for triggering the old_binding from save signals.
+        """
+        if action == Action.CREATE:
+            group_names = set()
+        else:
+            group_names = set(self.channel_names(instance))
+
+        # use a thread local dict to be safe...
+        if not hasattr(instance, '__instance_groups'):
+            instance.__instance_groups = threading.local()
+            instance.__instance_groups.observers = {}
+        if not hasattr(instance.__instance_groups, 'observers'):
+            instance.__instance_groups.observers = {}
+
+        instance.__instance_groups.observers[self] = group_names
+
+    def post_change_receiver(self, instance: Model, action: Action, **kwargs):
+        """
+        Triggers the old_binding to possibly send to its group.
+        """
+        if not hasattr(instance, '__instance_groups'):
+            old_group_names = set()
+        elif not hasattr(instance.__instance_groups, 'observers'):
+            old_group_names = set()
+        else:
+            old_group_names = instance.__instance_groups.observers[self]
+
+        if action == Action.DELETE:
+            new_group_names = set()
+        else:
+            new_group_names = set(self.channel_names(instance))
+
+        # if post delete, new_group_names should be []
+
+        # Django DDP had used the ordering of DELETE, UPDATE then CREATE for good reasons.
+        self.send_messages(
+            instance,
+            old_group_names - new_group_names,
+            Action.DELETE,
+            **kwargs
+        )
+        # the object has been updated so that its groups are not the same.
+        self.send_messages(
+            instance,
+            old_group_names & new_group_names,
+            Action.UPDATE,
+            **kwargs
+        )
+        #
+        self.send_messages(
+            instance,
+            new_group_names - old_group_names,
+            Action.CREATE,
+            **kwargs
+        )
+
+    def send_messages(self,
+                      instance: Model,
+                      group_names: Set[str],
+                      action: Action, **kwargs):
+        if not group_names:
+            return
+        message = self.serialize(instance, action, **kwargs)
+        channel_layer = get_channel_layer()
+        for group_name in group_names:
+            async_to_sync(channel_layer.group_send)(group_name, message)
+
+    async def subscribe(self, consumer: AsyncConsumer, *args, **kwargs):
+        for group_name in self.channel_names(*args, **kwargs):
+            await consumer.channel_layer.group_add(
+                group_name,
+                consumer.channel_name
+            )
+
+    def channel_names(self, instance=None, **kwargs):
+        model_label = '{}.{}'.format(
+            self.model_cls._meta.app_label.lower(),
+            self.model_cls._meta.object_name.lower()
+        ).lower().replace('_', '.')
+
+        # one channel for all updates.
+        yield '-model-{}'.format(
+            self.func.__name__.replace('_', '.'),
+            model_label,
+        )
+
+    def serialize(self, instance, action, **kwargs) -> Dict[str, Any]:
+        message = {}
+        if self._serializer:
+            message = self._serializer(self, instance, action, **kwargs)
+        else:
+            message['pk'] = instance.pk
+        message['type'] = self.func.__name__.replace('_', '.')
+        message['action'] = action.value
+        return message
+
+
+def model_observer(model: Type[Model], **kwargs):
+    return partial(ModelObserver, model_cls=model, kwargs=kwargs)
--- tests/test_observer.py
+++ tests/test_observer.py
@@ -6,7 +6,7 @@ from channels.layers import channel_layers
 from channels.testing import WebsocketCommunicator
 from django.contrib.auth import user_logged_in, get_user_model
 
-from channels_api.observer import observer
+from channels_api.observer import observer, model_observer
 
 
 @pytest.mark.django_db(transaction=True)
@@ -53,3 +53,51 @@ async def test_observer_wrapper(settings):
     response = await communicator.receive_json_from()
 
     assert {'message': {}} == response
+
+    await communicator.disconnect()
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_model_observer_wrapper(settings):
+    settings.CHANNEL_LAYERS={
+        "default": {
+            "BACKEND": "channels.layers.InMemoryChannelLayer",
+            "TEST_CONFIG": {
+                "expiry": 100500,
+            },
+        },
+    }
+
+    layer = channel_layers.make_test_backend(DEFAULT_CHANNEL_LAYER)
+
+    class TestConsumer(AsyncJsonWebsocketConsumer):
+
+        async def accept(self):
+            await TestConsumer.user_change.subscribe(self)
+            await super().accept()
+
+        @model_observer(get_user_model())
+        async def user_change(self, message):
+            await self.send_json(message)
+
+    communicator = WebsocketCommunicator(TestConsumer, "/testws/")
+
+    connected, _ = await communicator.connect()
+
+    assert connected
+
+    user = await database_sync_to_async(get_user_model().objects.create)(
+        username='test',
+        email='test@example.com'
+    )
+
+    response = await communicator.receive_json_from()
+
+    assert {
+               'action': 'create',
+               'pk': user.pk,
+               'type': 'user.change'
+           } == response
+
+    await communicator.disconnect()
