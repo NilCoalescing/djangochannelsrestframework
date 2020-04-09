31836983515e96d94e2202072278e4331659716a
Matthaus Woolard <matthaus.woolard@gmail.com>
2020-04-10 10:14:32 +1200

    Update lang version classifier

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         ],
     },
     classifiers=[
-        "Programming Language :: Python :: 3.7.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
 )
