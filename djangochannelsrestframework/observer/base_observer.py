3a0e3214296c731e2ad419b31dcc4952fada2ce7
lautarodapin <lautarodapin@gmail.com>
2021-04-20 19:26:03 -0300

    Better example for model_observer

--- djangochannelsrestframework/observer/base_observer.py
+++ djangochannelsrestframework/observer/base_observer.py
@@ -4,8 +4,6 @@ from uuid import uuid4
 
 from djangochannelsrestframework.consumers import AsyncAPIConsumer
 from djangochannelsrestframework.observer.utils import ObjPartial
-
-
 class BaseObserver:
     """Base observer class"""
 
@@ -37,9 +35,53 @@ class BaseObserver:
     def serializer(self, func):
         """Adds a Serializer to the model observer return.
 
+        .. note::
+            This is meant to use as a decorator.
+
         Examples:
-            .. code-bloc:: python
-                class MyConsumer(GeneriicAsyncAPIConsumer):
+            .. code-block:: python
+                
+                # models.py
+                from django.db import models
+                from django.contrib.auth.models import AbstractUser
+
+                class User(AbstractUser):
+                    pass
+
+                class Comment(models.Model):
+                    text = models.TextField()
+                    user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
+                    date = models.DatetimeField(auto_now_add=True)
+
+
+            .. code-block:: python
+
+                # serializers.py
+                from rest_framework import serializers
+                from .models import User, Comment
+
+                class UserSerializer(serializers.ModelSerializer):
+                    class Meta:
+                        model = User
+                        fields = ["id", "username", "email"]
+
+                class CommentSerializer(serializers.ModelSerializer):
+                    class Meta:
+                        model = Comment
+                        fields = ["id", "text", "user"]
+
+            .. code-block:: python
+
+                # consumers.py
+
+                from djangochannelsrestframework.consumers import GenericAsyncAPIConsumer
+                from djangochannelsrestframework.observer import model_observer
+                from djangochannelsrestframework.decorators import action
+
+                from .serializers import UserSerializer, CommentSerializer
+                from .models import User, Comment
+
+                class MyConsumer(GenericAsyncAPIConsumer):
                     queryset = User.objects.all()
                     serializer_class = UserSerializer
                     
@@ -54,6 +96,66 @@ class BaseObserver:
                     @action()
                     async def subscribe_to_comment_activity(self, **kwargs):
                         await self.comment_activity.subscribe()
+
+            Now we will have a websocket client in javascript listening to the messages, after subscribing to the comment activity.
+            This codeblock can be used it in the browser console.
+
+            .. code-block:: javascript
+
+                const ws = new WebSocket("ws://localhost:8000/ws/my-consumer/)
+                const ws.onopen = function(){
+                    ws.send(JSON.stringify({
+                        action: "subscribe_to_comment_activity",
+                        request_id: new Date().getTime(),
+                    }))
+                }
+                const ws.onmessage = function(e){
+                    console.log(e)
+                }
+
+            In the IPython shell we will create some comments for differnt users and in the browser console we will se the log.
+            
+            .. note::
+                At this point we should have some users in our database, otherwise create them
+
+            >>> from my_app.models import User, Comment
+            >>> user_1 = User.objects.get(pk=1)
+            >>> user_2 = User.objects.get(pk=2)
+            >>> Comment.objects.create(text="user 1 creates a new comment", user=user_1)
+
+            In the consol log we will se something like this:
+
+            .. code-block:: json
+
+                {
+                    action: "subscribe_to_comment_activity",
+                    errors: [],
+                    response_status: 200,
+                    request_id: 15606042,
+                    data: {
+                        id: 1,
+                        text: "user 1 creates a new comment",
+                        user: 1,
+                    },
+                }
+            Now we will create a comment with the user 2.
+            >>> Comment.objects.create(text="user 2 creates a second comment", user=user_2)
+            In the consol log we will se something like this:
+
+            .. code-block:: json
+
+                {
+                    action: "subscribe_to_comment_activity",
+                    errors: [],
+                    response_status: 200,
+                    request_id: 15606042,
+                    data: {
+                        id: 2,
+                        text: "user 2 creates a second comment",
+                        user: 2,
+                    },
+                }
+            As you can see in this example, we are subscribe to **ALL ACTIVITY** of the comment model.
         """
         self._serializer = func
         return self
