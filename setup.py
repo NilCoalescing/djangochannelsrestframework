fbbdfcab72fe7d9a0954f9c3dce0094a702d7e01
Matthaus Woolard <matthaus.woolard@gmail.com>
2022-02-21 14:26:14 +1300

    bump to v1

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import find_packages, setup
+from djangochannelsrestframework import __version__
 
 setup(
     name="djangochannelsrestframework",
-    version="0.3.0",
-    url="https://github.com/hishnash/djangochannelsrestframework",
-    author="Matthaus Woolard",
-    author_email="matthaus.woolard@gmail.com",
+    version=__version__,
+    url="https://github.com/lostmoa/djangochannelsrestframework",
+    author="LostMoa Limited",
+    author_email="info@lostmoa.com",
     description="RESTful API for WebSockets using django channels.",
     long_description=open("README.rst").read(),
     license="MIT",
@@ -22,9 +23,15 @@ setup(
     },
     python_requires=">=3.8",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
     ],
 )
