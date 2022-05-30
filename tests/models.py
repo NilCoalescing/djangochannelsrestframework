ef44784056eb5ee5ed86eccf2d8f82cecffa4dd5
Holger <holger@dissec.to>
2022-05-30 11:58:47 +0200

    * Add a test model with a custom pk / remove the test migrations    (created "on the fly" anyways / not necessary / would give an error with the changed model)  * Add a test for the "get correct subscription notification on instance creation" issue

--- tests/models.py
+++ tests/models.py
@@ -5,3 +5,9 @@ class TestModel(models.Model):
     """Simple model to test with."""
 
     name = models.CharField(max_length=255)
+
+
+class TestModelWithCustomPK(models.Model):
+    """Simple model with custom primary key to test with."""
+
+    name = models.CharField(max_length=255, primary_key=True)
