bf8c2ab0b904ae0f21e620ad0111ce208abb7b07
johnthagen <johnthagen@gmail.com>
2023-04-06 14:06:42 -0400

    Modernize supported Django and Python versions

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Django",
         "Topic :: Internet :: WWW/HTTP",
     ],
