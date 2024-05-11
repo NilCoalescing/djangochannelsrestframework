1b4c96b06233aa7ddfac343ad1f9ee9183f7bfe7
Mikhail Hubski <mikhail.hubski@leverx.com>
2024-05-11 09:10:17 +0200

    Add support of DRF OR, AND, NOT operands

--- tests/test_permission.py
+++ tests/test_permission.py
@@ -93,3 +93,135 @@ async def test_users_drf_permission_defaults(settings):
     connected, _ = await communicator.connect()
     assert connected
     assert called == {"has_permission": True}
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_users_drf_or_permission(settings):
+
+    called = {}
+
+    class TestPermissionA(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_a"] = False
+            return False
+
+    class TestPermissionB(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_b"] = True
+            return True
+
+    class AConsumer(AsyncAPIConsumer):
+        permission_classes = [TestPermissionA | TestPermissionB]
+        pass
+
+    # Test a normal connection
+    communicator = WebsocketCommunicator(AConsumer(), "/testws/")
+
+    connected, _ = await communicator.connect()
+    assert connected
+    assert called == {"has_permission_a": False, "has_permission_b": True}
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_users_drf_and_permission(settings):
+
+    called = {}
+
+    class TestPermissionA(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_a"] = True
+            return True
+
+    class TestPermissionB(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_b"] = True
+            return True
+
+    class AConsumer(AsyncAPIConsumer):
+        permission_classes = [TestPermissionA & TestPermissionB]
+        pass
+
+    # Test a normal connection
+    communicator = WebsocketCommunicator(AConsumer(), "/testws/")
+
+    connected, _ = await communicator.connect()
+    assert connected
+    assert called == {"has_permission_a": True, "has_permission_b": True}
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_users_drf_not_permission(settings):
+
+    called = {}
+
+    class TestPermission(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission"] = False
+            return False
+
+    class AConsumer(AsyncAPIConsumer):
+        permission_classes = [~TestPermission]
+        pass
+
+    # Test a normal connection
+    communicator = WebsocketCommunicator(AConsumer(), "/testws/")
+
+    connected, _ = await communicator.connect()
+    assert connected
+    assert called == {"has_permission": False}
+
+
+@pytest.mark.django_db(transaction=True)
+@pytest.mark.asyncio
+async def test_users_drf_complex_permission(settings):
+
+    called = {}
+
+    class TestPermissionA(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_a"] = False
+            return False
+
+    class TestPermissionB(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_b"] = True
+            return True
+
+    class TestPermissionC(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_c"] = True
+            return True
+
+    class TestPermissionD(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_d"] = True
+            return True
+
+    class TestEPermission(DRFBasePermission):
+        def has_permission(self, request, view):
+            called["has_permission_e"] = False
+            return False
+
+    class AConsumer(AsyncAPIConsumer):
+        permission_classes = (
+            TestPermissionA | TestPermissionB,
+            TestPermissionC & TestPermissionD,
+            ~TestEPermission,
+        )
+        pass
+
+    # Test a normal connection
+    communicator = WebsocketCommunicator(AConsumer(), "/testws/")
+
+    connected, _ = await communicator.connect()
+    assert connected
+    assert called == {
+        "has_permission_a": False,
+        "has_permission_b": True,
+        "has_permission_c": True,
+        "has_permission_d": True,
+        "has_permission_e": False
+    }
